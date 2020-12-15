6f52b1e085f13d88c9ed989af0e9895bb3c0419b
Bogdan Soucup <soucup.bogdan@gmail.com>
2020-12-15 22:30:55 +0200

    Transfered functions

--- frontend-new/impexceralux/scripts/Element.js
+++ frontend-new/impexceralux/scripts/Element.js
@@ -2,20 +2,6 @@ const path = "../date_impexcera/";
 var offsetTop = 0;
 var offsetLeft = 0;
 var magnified = 0;
-function getSearchParameters() {
-  var prmstr = window.location.search.substr(1);
-  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
-}
-
-function transformToAssocArray( prmstr ) {
-  var params = {};
-  var prmarr = prmstr.split("&");
-  for ( var i = 0; i < prmarr.length; i++) {
-      var tmparr = prmarr[i].split("=");
-      params[tmparr[0]] = tmparr[1];
-  }
-  return params;
-}
 
 function getFormatedStyle(photo) {
   console.log(window.innerWidth, window.innerHeight)
