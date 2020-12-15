83c9fae6d0548455def795fe38fa634c4df64db1
Bogdan Soucup <soucup.bogdan@gmail.com>
2020-12-15 20:34:20 +0200

    Added magnifier

--- frontend-new/impexceralux/scripts/Element.js
+++ frontend-new/impexceralux/scripts/Element.js
@@ -1,3 +1,7 @@
+const path = "../date_impexcera/";
+var offsetTop = 0;
+var offsetLeft = 0;
+var magnified = 0;
 function getSearchParameters() {
   var prmstr = window.location.search.substr(1);
   return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
@@ -13,21 +17,58 @@ function transformToAssocArray( prmstr ) {
   return params;
 }
 
+function getFormatedStyle(photo) {
+  console.log(window.innerWidth, window.innerHeight)
+  var img = new Image();
+  img.src = path + photo;
+  img.id = 'ana';
+  img.onload = function () {
+    var imge = document.getElementById('img1');
+    var newHeight = Math.min(500, img.height), newWidth= Math.min(500, img.width);
+    var newPosition = Math.floor(window.innerWidth / 2) - Math.floor(newWidth / 2);
+    imge.style = 'left: ' + newPosition + 'px; height: ' + newHeight + 'px; width: ' + newWidth + 'px;';
+   // imge.style = 'position: relative; left: ' + newPosition + 'px; height: ' + newHeight + 'px; width: ' + newWidth + 'px;';
+    var topPosition = imge.getBoundingClientRect().top - document.getElementById('portfolio').getBoundingClientRect().top;
+   // offsetTop = newPosition;
+    offsetLeft = Math.floor(topPosition / 2);
+  };
+}
+
+function activateMagnifier() {
+  if(!magnified) {
+    magnify('img1', 2, offsetTop, offsetLeft);
+    magnified = 1;
+  }
+  return false;
+}
+
 function loadImage(photo) {
-  var path = "../date_impexcera/";
   var parent = document.getElementById('portfolio');
-  var newElement = "<img class='left-element' style = 'left: 250px; position: relative' width='500' height='333' src=" + path + photo + " alt='Cinque Terre'>";
-  parent.appendChild(createElementFromHTML(newElement));
+  var newElement = createElementFromHTML("<img id=img1 src=" +
+                                         path + photo + " >");
+  parent.appendChild(newElement);
+  getFormatedStyle(photo);
 }
 
 function loadTitle(title) {
   document.getElementById('title').innerHTML = capitalizeFirstLetter(title);
 }
 
-function loadData(title) {
-  var parent = document.getElementById('portfolio');
-  var newElement = "<div style = 'position: absolute;' class='right-element' alt='Cinque Terre'>DADADADA</div>";
-  parent.appendChild(createElementFromHTML(newElement));
+function loadData(data) {
+  document.getElementById('0').innerHTML = data['id']
+  document.getElementById('1').innerHTML = data['categorie']
+  document.getElementById('2').innerHTML = data['culoare']
+  document.getElementById('3').innerHTML = data['dimensiuni']
+  $.ajax({
+    type: "GET",
+    url: 'http://localhost:3000/getElementByID?id=' + data['ColectionID'],
+    data: {},
+    success: function( record ) {
+      console.log(record)
+      document.getElementById('4').innerHTML = record['record']['colectie']
+    },
+    dataType: 'json'
+  });
 }
 
 function getElement() {
@@ -38,7 +79,7 @@ function getElement() {
     success: function( data ) {
       loadImage(data['record']['img']);
       loadTitle(data['record']['categorie']);
-      loadData("DADADA");
+      loadData(data['record']);
     },
     dataType: 'json'
   });
--- frontend-new/impexceralux/scripts/Utils.js
+++ frontend-new/impexceralux/scripts/Utils.js
@@ -13,3 +13,46 @@ async function sleep(time) {
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function magnify(imgID, zoom, leftOffset, topOffset) {
+    var img, glass, w, h, bw;
+    img = document.getElementById(imgID);
+    glass = document.createElement("DIV");
+    glass.style.position = 'relative';
+    glass.setAttribute("class", "img-magnifier-glass");
+    img.parentElement.insertBefore(glass, img);
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+    glass.addEventListener("touchmove", moveMagnifier);
+    img.addEventListener("touchmove", moveMagnifier);
+    function moveMagnifier(e) {
+      var pos, x, y;
+      e.preventDefault();
+      pos = getCursorPos(e);
+      x = pos.x;
+      y = pos.y;
+      if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+      if (x < w / zoom) {x = w / zoom;}
+      if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+      if (y < h / zoom) {y = h / zoom;}
+      glass.style.left = (x - w) + leftOffset + "px";
+      glass.style.top = (y - h) + topOffset + "px";
+      glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+    }
+    function getCursorPos(e) {
+      var a, x = 0, y = 0;
+      e = e || window.event;
+      a = img.getBoundingClientRect();
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return {x : x, y : y};
+    }
+  }
